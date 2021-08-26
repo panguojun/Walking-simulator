ce091444750505822b7c17f74d2e7ed9a9f49246
panguojun <panguojun@users.noreply.github.com>
2021-08-26 09:37:44 +0800

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,18 @@
+require 'com'
+mod(0)
+
+--ent(180, "main3D.lua", 0.5, 0,0,0, 0, 1,0)
+
+for i = 0, 70 do
+	local v = V(rnd(-5,5),rnd(-5,5),rnd(-5,5))
+	v = norm(v);
+	local p = v * 20.0
+	ent(101 + i, "tree.lua", 0.05, p.x, p.y, p.z, v.x, v.y, v.z);
+end
+
+for i = 0, 50 do
+	local v = V(rnd(-5,5),rnd(-5,5),rnd(-5,5))
+	v = norm(v);
+	local p = v * 25.0
+	ent(181 + i, "shell.lua", 0.02, p.x, p.y, p.z, v.x, v.y, v.z);
+end
